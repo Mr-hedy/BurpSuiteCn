04a6d1a5ccf85ae488175758d437d038b6c2e4ab
funk <826262043@qq.com>
2019-09-18 17:05:00 +0800

    更新代码

--- src/zhcn/BurpJavaaGentMain.java
+++ src/zhcn/BurpJavaaGentMain.java
@@ -10,7 +10,7 @@ import java.io.*;
 public class BurpJavaaGentMain {
 
 	public static void main(String[] args) throws IOException {
-		System.out.println("请使用 javaagent方式运行\n");
+		System.out.println("请使用 javaagent方式运行 \n");
 		System.out.println("Linux Mac 下：\n");
 		System.out.println("java -javaagent:BurpSuiteCn.jar -Xbootclasspath/p:burp-loader-keygen.jar  -Xmx1024m -jar burpsuite_pro_v1.x.x.jar\n");
 		System.out.println("Windwos 下：\n");
